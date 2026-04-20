b328e4188c79df038d9c7c6dc46f2d1061003aae
Erick <erickyanezsantana@gmail.com>
2026-04-20 08:43:40 -0500

    update

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
   build: {
     rollupOptions: {
       input: {
         main: './index.html',
-        foro: './foro.html'
+        foro: './foro.html',
+        donar: './donar.html',
+        privacidad: './privacidad.html',
+        terminos: './terminos.html'
       }
     }
   }
